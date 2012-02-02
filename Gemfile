c02a57fb930ef59cd97b41494283229b62991443
John Knotts <me@johnknotts.com>
2012-02-01 19:49:30 -0800

    Added a User factory and tests for the show page

--- Gemfile
+++ Gemfile
@@ -16,4 +16,5 @@ group :test do
   gem 'autotest-rails-pure', '4.1.2'
   gem 'autotest-fsevent', '0.2.4'
   gem 'autotest-growl', '0.2.16'
+  gem 'factory_girl_rails', '1.0'
 end
---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                   "John Knotts"
+  user.email                  "jk@example.com"
+  user.password               "foobar"
+  user.password_confirmation  "foobar"
+end

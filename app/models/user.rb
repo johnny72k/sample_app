d379e08ace8d142182c4ceec59182d0b39a558f3
John Knotts <me@johnknotts.com>
2012-02-08 21:34:57 -0800

    Working user signin

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,8 +21,14 @@ class User < ActiveRecord::Base
 
   def User.authenticate(email, submitted_password)
     user = find_by_email(email)
-    return nil  if user.nil?
-    return user if user.has_password?(submitted_password)
+    (user && user.has_password?(submitted_password)) ? user : nil
+  end
+  
+  class << self # this syntax achieves the same class method as the syntax above
+    def authenticate_with_salt(id, cookie_salt)
+      user = find_by_id(id)
+      (user && user.salt == cookie_salt) ? user : nil
+    end
   end
 
   private
@@ -45,6 +51,7 @@ class User < ActiveRecord::Base
     end
 end
 
+
 # == Schema Information
 #
 # Table name: users
@@ -55,5 +62,6 @@ end
 #  created_at         :datetime
 #  updated_at         :datetime
 #  encrypted_password :string(255)
+#  salt               :string(255)
 #
 

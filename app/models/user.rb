1e4a89eabf7e32cdf75fe5d112a14d78715ea2da
John Knotts <me@johnknotts.com>
2012-02-15 19:53:12 -0800

    Can now destroy users (as admins)

--- app/models/user.rb
+++ app/models/user.rb
@@ -52,6 +52,7 @@ class User < ActiveRecord::Base
 end
 
 
+
 # == Schema Information
 #
 # Table name: users
@@ -63,5 +64,6 @@ end
 #  updated_at         :datetime
 #  encrypted_password :string(255)
 #  salt               :string(255)
+#  admin              :boolean         default(FALSE)
 #
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -151,7 +151,27 @@ describe User do
       end
     end
   end
+  
+  describe "admin attribute" do
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+    
+    it "should respond to admin" do
+      @user.should respond_to(:admin)
+    end
+    
+    it "should not be an admin by default" do
+      @user.should_not be_admin
+    end
+    
+    it "should be convertible to an admin" do
+      @user.toggle!(:admin)
+      @user.should be_admin
+    end
+  end
 end
+
 
 
 # == Schema Information
@@ -165,5 +185,6 @@ end
 #  updated_at         :datetime
 #  encrypted_password :string(255)
 #  salt               :string(255)
+#  admin              :boolean         default(FALSE)
 #
 
